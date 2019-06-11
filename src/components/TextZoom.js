ce35fb87ab57edad6f6944ccf7472b7f5644c531
Ben Hays <ben@kajabi.com>
2019-06-11 14:24:30 -0700

    Added basic logo.

--- src/components/TextZoom.js
+++ src/components/TextZoom.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import $ from "jquery";
 import TweenMax from "gsap/TweenMax";
 
+import { ReactComponent as Logo } from "../images/kajabi_logo.svg";
+
 class TextZoom extends Component {
   render() {
     return (
-      <h1>TextZoom</h1>
+      <section className="text-zoom">
+        <div className="logo">
+          <Logo className="logo__svg" />
+        </div>
+      </section>
     );
   }
 }
