ed87aa8c94f65989bba28d25cd69f3a145044cdf
Ben Hays <ben@kajabi.com>
2019-06-11 14:08:11 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import TextZoom from "./TextZoom";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TextZoom />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TextZoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import $ from "jquery";
+import TweenMax from "gsap/TweenMax";
+
+class TextZoom extends Component {
+  render() {
+    return (
+      <h1>TextZoom</h1>
+    );
+  }
+}
+
+export default TextZoom;
