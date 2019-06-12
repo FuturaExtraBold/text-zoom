e3555e3f3afd7c297b86779ecd04c30c55875182
Ben Hays <ben@kajabi.com>
2019-06-12 11:55:22 -0700

    Trying to get the animation working.

--- src/components/TextZoom.js
+++ src/components/TextZoom.js
@@ -5,12 +5,21 @@ import TweenMax from "gsap/TweenMax";
 import { ReactComponent as Logo } from "../images/kajabi_logo.svg";
 
 class TextZoom extends Component {
+
+  componentDidMount() {
+    console.log("TextZoom componentDidMount");
+    let $logo = $(".logo");
+    // TweenMax.set($logo, { xPercent: "-50", yPercent: "-50" });
+    // TweenMax.set($(".logo__path"), { attr: { x: "50%", y: "" }});
+    // TweenMax.to($(".text-zoom__bg"), 100, { scale: 100, ease: "linear", delay: 1 });
+  }
+
   render() {
+    const bgImgUrl = require(`../images/bg.jpg`);
     return (
       <section className="text-zoom">
-        <div className="logo">
-          <Logo className="logo__svg" />
-        </div>
+        <div className="text-zoom__bg"></div>
+        <Logo />
       </section>
     );
   }
