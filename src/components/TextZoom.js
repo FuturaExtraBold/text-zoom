ea402ff25215211156db9bcef978b9defde3b25c
Ben Hays <ben@kajabi.com>
2019-06-17 10:18:27 -0700

    Working on scaling the text properly.

--- src/components/TextZoom.js
+++ src/components/TextZoom.js
@@ -3,15 +3,47 @@ import $ from "jquery";
 import TweenMax from "gsap/TweenMax";
 
 import { ReactComponent as Logo } from "../images/kajabi_logo.svg";
+import { POINT_CONVERSION_COMPRESSED } from "constants";
 
 class TextZoom extends Component {
 
   componentDidMount() {
+
     console.log("TextZoom componentDidMount");
-    let $logo = $(".logo");
-    // TweenMax.set($logo, { xPercent: "-50", yPercent: "-50" });
-    // TweenMax.set($(".logo__path"), { attr: { x: "50%", y: "" }});
-    // TweenMax.to($(".text-zoom__bg"), 100, { scale: 100, ease: "linear", delay: 1 });
+
+    var canvas = document.getElementById("my-canvas");
+
+    // Get the device pixel ratio, falling back to 1.
+    var dpr = window.devicePixelRatio || 1;
+    console.log("dpr:", dpr);
+
+    // Get the size of the canvas in CSS pixels.
+    var rect = canvas.getBoundingClientRect();
+    console.log("rect:", rect);
+
+    // Give the canvas pixel dimensions of their CSS size * the device pixel ratio.
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+
+    var ctx = canvas.getContext("2d");
+
+    // Scale all drawing operations by the dpr, so you don't have to worry about the difference.
+    ctx.scale(dpr, dpr);
+
+    // ctx.lineWidth = 5;
+    // ctx.beginPath();
+    // ctx.moveTo(100, 100);
+    // ctx.lineTo(200, 200);
+    // ctx.stroke();
+
+    ctx.fillStyle = "rgba(255, 255, 255, 1)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "160px Proxima Nova";
+    ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    ctx.globalCompositeOperation = "destination-out";
+    // ctx.fillText(heading.innerText, canvas.width / 4, canvas.height / 2);
+    ctx.fillText("Hello World!", 10, 50);
+
   }
 
   render() {
@@ -19,7 +51,7 @@ class TextZoom extends Component {
     return (
       <section className="text-zoom">
         <div className="text-zoom__bg"></div>
-        <Logo />
+        <canvas id="my-canvas"></canvas>
       </section>
     );
   }
