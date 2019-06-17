f6d61eea7a03cc9fecf48c23249ac0c33ecd6fd8
Ben Hays <ben@kajabi.com>
2019-06-17 16:22:40 -0700

    Working from Apple script.

--- src/components/TextZoom.js
+++ src/components/TextZoom.js
@@ -1,67 +1,70 @@
 import React, { Component } from "react";
-import $ from "jquery";
-import * as PIXI from "pixi.js";
-import TweenMax from "gsap";
-import PixiPlugin from "gsap/PixiPlugin";
-import { maxHeaderSize } from "http";
+
+import { ReactComponent as Logo } from "../images/ipad_logo.svg";
 
 class TextZoom extends Component {
 
   componentDidMount() {
     console.log("TextZoom componentDidMount");
+    let obj = {};
+    obj.canvas = document.querySelector("canvas");
+    obj.ctx = obj.canvas.getContext("2d");
+    obj.scale = 1;
+    obj.metrics = {
+      width: 0,
+      height: 0,
+      retinaScale: window.devicePixelRatio,
+      canvasWidth: 100,
+      canvasHeight: 100,
+      svgWidth: 0,
+      svgHeight: 0
+    };
+    console.log("obj:", obj);
 
-    let $window = $(window);
-
-    const app = new PIXI.Application({
-      width: $window.outerWidth(),
-      height: $window.outerHeight(),
-      backgroundColor: 0xffffff,
-      resolution: window.devicePixelRatio || 1
-    });
-    document.querySelector(".zoom__positioner").appendChild(app.view);
-
-    app.stage.interactive = true;
-
-    const bg = PIXI.Sprite.from(require("../images/bg.jpg"));
-    app.stage.addChild(bg);
-
-    // var beeSvg = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/bee.svg";
-
-    const kajabiLogo = PIXI.Texture.from(require(`../images/ipad_logo.svg`), undefined, undefined, 1.0);
-    const mask = new PIXI.Sprite(kajabiLogo);
-    // mask.anchor.set(0.5);
-    mask.x = 300;
-    mask.y = 300;
-
-    bg.mask = mask;
-
-    app.stage.addChild(mask);
-
-    const target = new PIXI.Point();
-
-    reset();
-
-    function reset() {
-      target.x = Math.floor(Math.random() * 550);
-      target.y = Math.floor(Math.random() * 300);
+    function draw() {
+      getSVG();
+      drawBackgroundRect();
+      drawPath();
     }
 
-    app.ticker.add(() => {
-      // mask.x += (target.x - mask.x) * 0.1;
-      // mask.y += (target.y - mask.y) * 0.1;
-      // mask.scale.x += 0.001;
-      // mask.scale.y += 0.001;
-      if (Math.abs(mask.x - target.x) < 1) {
-        reset();
-      }
-    });
+    function drawBackgroundRect() {
+      console.log("drawBackgroundRect");
+      // obj.ctx.globalCompositeOperation = "source-over";
+      // obj.ctx.setTransform(1, 0, 0, 1, 0, 0);
+      // obj.ctx.fillStyle = "rgba(255, 0, 0, 1.0)";
+      // obj.ctx.fillRect(0, 0, obj.metrics.canvasWidth, obj.metrics.canvasHeight);
+    }
+
+    function drawPath() {
+      console.log("drawPath");
+    }
+
+    function getSVG() {
+      console.log("getSVG");
+      let t = document.querySelector("svg");
+      let e = t.attributes.viewBox.value.split(" ").splice(2, 2).map(parseFloat);
+      t.setAttribute("width", e[0]);
+      t.setAttribute("height", e[1]);
+      let r = new Blob([t.outerHTML], {
+        type: "image/svg+xml"
+      });
+      let n = (window.URL || window.webkitURL).createObjectURL(r);
+      let img = new Image;
+      img.src = n;
+      console.log("img:", img);
+      img.addEventListener("load", function() {
+        console.log("image loaded");
+      });
+    }
+
+    draw();
   }
 
   render() {
     return (
       <section className="zoom">
-        <div className="zoom__positioner">
-        </div>
+        <canvas id="canvie"></canvas>
+        <Logo />
       </section>
     );
   }
